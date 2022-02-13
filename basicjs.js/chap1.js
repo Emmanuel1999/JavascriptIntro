cd11c6588d857213b526ba543669874f14c4047d
Emmanuel1999 <emmanuelopatola99@gmail.com>
2022-02-13 10:13:08 +0100

    Initial Commit

---
+++ basicjs.js/chap1.js
@@ -0,0 +1,114 @@
+//commenting in javascript 
+// Comment
+/* This is a comment*/
+
+//declare Javascript variables 
+var myName;
+
+//storing values with the assignment operator
+var a;
+a =7;
+
+//Assigning the Value of One Variable to Another--
+var a;
+a = 7;
+var b;
+// Only change code below this line
+b=a;
+
+//Initializing Variables with the Assignment Operator--
+var a =9;
+
+//Declare String Variables
+var myFirstName;
+var myLastName;
+myFirstName = "Emmanuel";
+myLastName = "Opatola";
+
+//Understanding Uninitialized Variables --
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Understanding Case Sensitivity in Variables--
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Explore Differences Between the var and let Keywords--
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//Add Two Numbers with JavaScript
+const sum = 10 + 10;
+
+//Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+//Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+//Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+//Increment a Number with JavaScript
+let myVar = 87;
+// Only change code below this line
+myVar++;
+
+//Decrement a Number with JavaScript
+let myVar = 11;
+// Only change code below this line
+myVar--;
+
+//Create Decimal Numbers with JavaScript
+const myDecimal = 5.7;
+
+//Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+//Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0;
+
+//Finding a Remainder in JavaScript
+const remainder = 11%3;
+
+//Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
